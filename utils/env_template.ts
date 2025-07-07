1a8f08cb236ccd2f85f7dafc316f35bb823c80e1
Celthim <34304819+Celthim@users.noreply.github.com>
2025-07-07 12:06:20 +0200

    Add login + comments + send comment + refresh dates

---
+++ utils/env_template.ts
@@ -0,0 +1,4 @@
+export const config = {
+    "API_URL" : "URL PRIVATE API",
+    "AVATAR_API_URL" : "https://api.dicebear.com/9.x/personas/svg?backgroundType=gradientLinear&backgroundRotation=360,0&backgroundColor=ffd5dc,ffdfbf,d1d4f9,c0aede,b6e3f4&seed=",
+}
